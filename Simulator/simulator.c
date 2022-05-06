0a6d5c7db70ebe30322079fd36888be155bb1a34
SahasR <sahasrenuja@gmail.com>
2022-05-06 20:42:03 +0530

    Finally completely working Assignment 1

--- Simulator/simulator.c
+++ Simulator/simulator.c
@@ -10,14 +10,54 @@
 pthread_mutex_t fileReadLock;
 pthread_cond_t fileReadCondition;
 volatile int fileRead = 0;
-volatile int* buffer1;
+int* buffer1;
 volatile int buffer1Size;
-volatile int* buffer2;
+int buffer2;
+pthread_mutex_t bufferLock;
+pthread_cond_t bufferLockCondition;
+pthread_cond_t writeCondition;
+volatile int written = 0;
+volatile int totalWritten = 0;
+volatile int returnAlgo;
+pthread_mutex_t newLoopLock;
+pthread_cond_t newLoopCond;
+volatile int nextLoop;
 
 void cleanUpHandler(void* lock){
     pid_t x = syscall(__NR_gettid);
     printf("%d has terminated\n", x);
     pthread_mutex_unlock((pthread_mutex_t *)lock);
+}
+
+void writeBuffer(int seek, int type){
+    pthread_mutex_lock(&bufferLock);
+    while (written == 1){
+        pthread_cond_wait(&bufferLockCondition, &bufferLock);
+    }
+    buffer2 = seek;
+    returnAlgo = type;
+    totalWritten++;
+    written = 1;
+    pthread_cond_signal(&bufferLockCondition);
+    pthread_mutex_unlock(&bufferLock);
+}
+
+void waitLock(){
+    pthread_mutex_lock(&fileReadLock);
+    while(fileRead == 0){ 
+        pthread_cond_wait(&fileReadCondition, &fileReadLock);
+    }
+    printf("\n");
+    pthread_mutex_unlock(&fileReadLock);
+}
+
+void waitNextLoop(){
+    pthread_mutex_lock(&newLoopLock);
+    while (nextLoop == 0){
+        pthread_cond_wait(&newLoopCond, &newLoopLock);
+    }
+    printf("\n");
+    pthread_mutex_unlock(&newLoopLock);
 }
 
 void * childThreadA(void * tid){
@@ -26,24 +66,28 @@ void * childThreadA(void * tid){
     int oldtype;
     int * temp;
     int seek;
+
     pthread_cleanup_push(cleanUpHandler, &fileReadLock);
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &oldtype);
-    pthread_mutex_lock(&fileReadLock);
-    while(fileRead == 0){ 
-        pthread_cond_wait(&fileReadCondition, &fileReadLock);
+
+    for (;;){
+        
+        waitLock();
+
+        temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
+        for (i = 3; i < buffer1Size; i++){
+            temp[i-3] = buffer1[i];
+        }
+        seek = firstComeFirstServe(temp, buffer1Size -3, buffer1[1]);
+        
+        writeBuffer(seek, 1);
+
+        free(temp);
+
+        waitNextLoop();
     }
-    pthread_mutex_unlock(&fileReadLock);
-    
-    temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
-    for (i = 3; i < buffer1Size; i++){
-        temp[i-3] = buffer1[i];
-    }
-    seek = firstComeFirstServe(temp, buffer1Size -3, buffer1[1]);
-    printf("FCFS : %d\n", seek);
-    free(temp);
-    
     pthread_cleanup_pop(0);
-    pthread_exit(NULL);
+
 }
 
 void * childThreadB(void * tid){
@@ -53,22 +97,24 @@ void * childThreadB(void * tid){
     int seek;
     pthread_cleanup_push(cleanUpHandler, &fileReadLock);
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &oldtype);
-    pthread_mutex_lock(&fileReadLock);
-    while(fileRead == 0){ 
-        pthread_cond_wait(&fileReadCondition, &fileReadLock);
+
+    for (;;){
+        
+        waitLock();
+        
+        temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
+        for (i = 3; i < buffer1Size; i++){
+            temp[i-3] = buffer1[i];
+        }
+        seek = shortestSeekTimeFirst(temp, buffer1Size -3, buffer1[1], buffer1[0]);
+
+        writeBuffer(seek, 2);
+
+        free(temp);
+
+        waitNextLoop();
     }
-    pthread_mutex_unlock(&fileReadLock);
-    
-    temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
-    for (i = 3; i < buffer1Size; i++){
-        temp[i-3] = buffer1[i];
-    }
-    seek = shortestSeekTimeFirst(temp, buffer1Size -3, buffer1[1], buffer1[0]);
-    printf("SSTF : %d\n", seek);
-    free(temp);
-    
     pthread_cleanup_pop(0);
-    pthread_exit(NULL);
 }
 
 void * childThreadC(void * tid){
@@ -78,22 +124,23 @@ void * childThreadC(void * tid){
     int i;
     pthread_cleanup_push(cleanUpHandler, &fileReadLock);
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &oldtype);
-    pthread_mutex_lock(&fileReadLock);
-    while(fileRead == 0){ 
-        pthread_cond_wait(&fileReadCondition, &fileReadLock);
+    for (;;){
+         
+        waitLock();
+        
+        temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
+        for (i = 3; i < buffer1Size; i++){
+            temp[i-3] = buffer1[i];
+        }
+        seek = scan(temp, buffer1Size -3, buffer1[1], buffer1[2], buffer1[0]);
+
+        writeBuffer(seek, 3);
+
+        free(temp);
+
+        waitNextLoop();
     }
-    pthread_mutex_unlock(&fileReadLock);
-    
-    temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
-    for (i = 3; i < buffer1Size; i++){
-        temp[i-3] = buffer1[i];
-    }
-    seek = scan(temp, buffer1Size -3, buffer1[1], buffer1[2], buffer1[0]);
-    printf("SCAN : %d\n", seek);
-    free(temp);
-    
     pthread_cleanup_pop(0);
-    pthread_exit(NULL);
 }
 
 void * childThreadD(void * tid){
@@ -104,22 +151,23 @@ void * childThreadD(void * tid){
 
     pthread_cleanup_push(cleanUpHandler, &fileReadLock);
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &oldtype);
-    pthread_mutex_lock(&fileReadLock);
-    while(fileRead == 0){ 
-        pthread_cond_wait(&fileReadCondition, &fileReadLock);
-    }
-    pthread_mutex_unlock(&fileReadLock);
+    for (;;){
+        
+        waitLock();
 
-    temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
-    for (i = 3; i < buffer1Size; i++){
-        temp[i-3] = buffer1[i];
+        temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
+        for (i = 3; i < buffer1Size; i++){
+            temp[i-3] = buffer1[i];
+        }
+        seek = cscan(temp, buffer1Size -3, buffer1[1], buffer1[2], buffer1[0]);
+
+        writeBuffer(seek, 4);
+
+        free(temp);
+
+        waitNextLoop();
     }
-    seek = cscan(temp, buffer1Size -3, buffer1[1], buffer1[2], buffer1[0]);
-    printf("C-SCAN : %d\n", seek);
-    free(temp);
-    
     pthread_cleanup_pop(0);
-    pthread_exit(NULL);
 }
 
 void * childThreadE(void * tid){
@@ -130,22 +178,23 @@ void * childThreadE(void * tid){
 
     pthread_cleanup_push(cleanUpHandler, &fileReadLock);
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &oldtype);
-    pthread_mutex_lock(&fileReadLock);
-    while(fileRead == 0){ 
-        pthread_cond_wait(&fileReadCondition, &fileReadLock);
-    }
-    pthread_mutex_unlock(&fileReadLock);
-    
-    temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
-    for (i = 3; i < buffer1Size; i++){
-        temp[i-3] = buffer1[i];
-    }
-    seek = look(temp, buffer1Size -3, buffer1[1], buffer1[2]);
-    printf("LOOK : %d\n", seek);
-    free(temp);
+    for (;;){
+        
+        waitLock();
+        
+        temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
+        for (i = 3; i < buffer1Size; i++){
+            temp[i-3] = buffer1[i];
+        }
+        seek = look(temp, buffer1Size -3, buffer1[1], buffer1[2]);
 
+        writeBuffer(seek, 5);
+
+        free(temp);
+
+        waitNextLoop();
+    }
     pthread_cleanup_pop(0);
-    pthread_exit(NULL);
 }
 
 void * childThreadF(void * tid){
@@ -155,22 +204,26 @@ void * childThreadF(void * tid){
     int i;
     pthread_cleanup_push(cleanUpHandler, &fileReadLock);
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &oldtype);
-    pthread_mutex_lock(&fileReadLock);
-    while(fileRead == 0){ 
-        pthread_cond_wait(&fileReadCondition, &fileReadLock);
-    }
-    pthread_mutex_unlock(&fileReadLock);
+    for (;;){
+        pthread_mutex_lock(&fileReadLock);
+        while(fileRead == 0){ 
+            pthread_cond_wait(&fileReadCondition, &fileReadLock);
+        }
+        pthread_mutex_unlock(&fileReadLock);
 
-    temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
-    for (i = 3; i < buffer1Size; i++){
-        temp[i-3] = buffer1[i];
+        temp = (int* )malloc((buffer1Size - 3)*sizeof(int));
+        for (i = 3; i < buffer1Size; i++){
+            temp[i-3] = buffer1[i];
+        }
+        seek = clook(temp, buffer1Size -3, buffer1[1], buffer1[2]);
+
+        writeBuffer(seek, 6);
+
+        free(temp);
+
+        waitNextLoop();
     }
-    seek = clook(temp, buffer1Size -3, buffer1[1], buffer1[2]);
-    printf("C-LOOK : %d\n", seek);
-    free(temp);
-    
     pthread_cleanup_pop(0);
-    pthread_exit(NULL);
 }
 
 int main(int argc, char* argv[]){
@@ -197,18 +250,22 @@ int main(int argc, char* argv[]){
     pthread_t tE;
     pthread_t tF;
 
+    pthread_mutex_init(&fileReadLock, NULL);
+    pthread_cond_init(&fileReadCondition, NULL);
+    pthread_mutex_init(&bufferLock, NULL);
+    pthread_cond_init(&bufferLockCondition, NULL);
+    pthread_mutex_init(&newLoopLock, NULL);
+    pthread_cond_init(&newLoopCond, NULL);
+
+    pthread_create(&tA, NULL, childThreadA, (void *)&tA);
+    pthread_create(&tB, NULL, childThreadB, (void *)&tB);
+    pthread_create(&tC, NULL, childThreadC, (void *)&tC);
+    pthread_create(&tD, NULL, childThreadD, (void *)&tD);
+    pthread_create(&tE, NULL, childThreadE, (void *)&tE);
+    pthread_create(&tF, NULL, childThreadF, (void *)&tF);
+    sleep(2);
+
     do {
-        pthread_mutex_init(&fileReadLock, NULL);
-        pthread_cond_init(&fileReadCondition, NULL);
-
-        pthread_create(&tA, NULL, childThreadA, (void *)&tA);
-        pthread_create(&tB, NULL, childThreadB, (void *)&tB);
-        pthread_create(&tC, NULL, childThreadC, (void *)&tC);
-        pthread_create(&tD, NULL, childThreadD, (void *)&tD);
-        pthread_create(&tE, NULL, childThreadE, (void *)&tE);
-        pthread_create(&tF, NULL, childThreadF, (void *)&tF);
-        sleep(2);
-
         printf("Disk Schedular Simulation: ");
         scanf("%s", input);
 
@@ -217,7 +274,6 @@ int main(int argc, char* argv[]){
             if (status == 1){
                 a = convert(data, &length);
                 buffer1 = (int* )malloc((length+3)*sizeof(int));
-                buffer2 = (int* )malloc(6*sizeof(int));
                 buffer1[0] = totalCylinders;
                 buffer1[1] = currPosition;
                 buffer1[2] = prevPosition;
@@ -225,19 +281,65 @@ int main(int argc, char* argv[]){
                     buffer1[i+3] = a[i];
                 }
                 buffer1Size = length + 3;
+                pthread_mutex_lock(&newLoopLock);
+                nextLoop = 0;
+                pthread_cond_broadcast(&newLoopCond);
+                pthread_mutex_unlock(&newLoopLock);
+
                 pthread_mutex_lock(&fileReadLock);
                 fileRead = 1;
-                pthread_cond_signal(&fileReadCondition);
                 pthread_cond_broadcast(&fileReadCondition);
                 pthread_mutex_unlock(&fileReadLock);
-            } else {
-                printf("File not found so cancelling threads!\n");
-                pthread_cancel(tA);
-                pthread_cancel(tB);
-                pthread_cancel(tC);
-                pthread_cancel(tD);
-                pthread_cancel(tE);
-                pthread_cancel(tF);
+
+                while (totalWritten != 6){
+                    pthread_mutex_lock(&bufferLock);
+                    while (written == 0){
+                    pthread_cond_wait(&bufferLockCondition, &bufferLock);
+                    }
+                
+                    switch (returnAlgo){
+                        case 1:
+                            fcfs = buffer2;
+                            break;
+                        case 2:
+                            sstf = buffer2;
+                            break;
+                        case 3:
+                            sc = buffer2;
+                            break;
+                        case 4:
+                            csc = buffer2;
+                            break;
+                        case 5:
+                            lk = buffer2;
+                            break;
+                        case 6:
+                            clk = buffer2;
+                            break;
+                    }
+                    
+                    written = 0;
+                    pthread_cond_signal(&bufferLockCondition);
+                    pthread_mutex_unlock(&bufferLock);
+                    sleep(0.5);
+                }
+
+                printf("For %s:\n", input);
+                printf("FCFS: %d\n", fcfs);
+                printf("SSTF: %d\n", sstf);
+                printf("SCAN: %d\n", sc);
+                printf("CSCAN: %d\n", csc);
+                printf("LOOK: %d\n", lk);
+                printf("CLOOK: %d\n", clk);
+
+                written = 0;
+                totalWritten = 0;
+                fileRead = 0;
+
+                pthread_mutex_lock(&newLoopLock);
+                nextLoop = 1;
+                pthread_cond_broadcast(&newLoopCond);
+                pthread_mutex_unlock(&newLoopLock);
             }
         } else {
             pthread_cancel(tA);
@@ -247,21 +349,10 @@ int main(int argc, char* argv[]){
             pthread_cancel(tE);
             pthread_cancel(tF);
         }
-        pthread_join(tA, NULL);
-        pthread_join(tB, NULL);
-        pthread_join(tC, NULL);
-        pthread_join(tD, NULL);
-        pthread_join(tE, NULL);
-        pthread_join(tF, NULL);
-
-        pthread_mutex_destroy(&fileReadLock);
-        pthread_cond_destroy(&fileReadCondition);
-        fileRead = 0;
+        sleep(2);
     } while (strcmp(input, "QUIT") != 0);
 
-    
-    
-    
+    free(buffer1);
 
     return 0;
 }
